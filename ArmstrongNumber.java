1847a9b4af28aa0f45da64932f6f205802458adf
Niall Campbell <campbellniall@gmail.com>
2019-08-13 15:53:25 +0100

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.javaquestions;
 
 import java.util.Scanner;
@@ -14,7 +10,7 @@ import java.util.Scanner;
  *  e.g. 371
  * 
  *  
- * @author niall.campbell@ammeon.com
+ * 
  */
 public class ArmstrongNumber {
     
