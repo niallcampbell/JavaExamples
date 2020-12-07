55fbdd59acc48b740878dbf55875f68b41e105e5
Niall Campbell <niallcampbell@Nialls-MacBook-Air.local>
2020-12-07 11:05:15 +0000

    Added generic max value method

--- Generics/Generics.java
+++ Generics/Generics.java
@@ -14,6 +14,25 @@ public class Generics
         System.out.println();
     }
     
+    public static <E extends Comparable<E>> E maximumValue(E [] array){
+        
+        if(array.length == 0){
+            return null;
+        }
+        
+        E max = array[0];
+        
+        for(int i = 1; i < array.length; i++){
+            
+            if(array[i].compareTo(max) > 0){
+                max = array[i];
+            }
+            
+        }
+        
+        return max;
+    }
+    
     public static void main(String args[]){
         
         Integer [] arr1 = {1,2,3};
@@ -29,5 +48,10 @@ public class Generics
         System.out.println("String array:");
         printArray(arr3);
         
+        Integer [] arr4 = {32, 45, 21, 89, 56};
+        System.out.println(maximumValue(arr4));
+        
+        Double [] arr5 = {56.0, 34.4, 56.1, 24.3, 14.9};
+        System.out.println(maximumValue(arr5));
     }
 }
