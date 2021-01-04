c343d460d32f12471aa339442bf45161b71b2f4b
Niall Campbell <campbellniall@gmail.com>
2021-01-04 16:07:06 +0000

    Find largest and smallest values in an array

--- Arrays/src/DuplicateValue.java
+++ Arrays/src/DuplicateValue.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.MissingFormatArgumentException;
 import java.util.Map.Entry;
 
 public class DuplicateValue {
---
+++ Arrays/src/MiscQs.java
@@ -0,0 +1,44 @@
+public class MiscQs {
+    
+    /**
+     *  Find the largest number in an array. 
+     */
+
+    public static int largestValue(int[] arr){
+
+        int largest = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+
+        return largest;
+    }
+
+    /**
+     *  Find the smallest value in an array. 
+     */
+
+    public static int smallestValue(int[] arr){
+
+        int smallest = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] < smallest){
+                smallest = arr[i];
+            }
+        }
+
+        return smallest;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {23, -45, 78, 92, 12, 45};
+        System.out.println("Smallest Value: " + smallestValue(arr));
+        System.out.println("Largest Value: " + largestValue(arr));
+    }
+
+}
