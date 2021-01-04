253b4d220a92fcc577fcf5ec22888187b58dde96
Niall Campbell <campbellniall@gmail.com>
2021-01-04 12:25:57 +0000

    Added code to count the number of vowels in a string

---
+++ StringPractice/src/NumberOfVowels.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class NumberOfVowels {
+    
+    /**
+     *  Count and print the number of vowels in a String. 
+     */
+
+     public static void countNumberOfVowels(String word){
+
+        char[] chars = word.toCharArray();
+
+        HashMap <Character, Integer> map = new HashMap<Character, Integer>();
+
+        for(int i = 0; i < chars.length; i++){
+            if(map.containsKey(chars[i])){
+                map.put(chars[i], map.get(chars[i]) + 1);
+            } else {
+                map.put(chars[i], 1);
+            }
+        }
+
+        for(Entry<Character, Integer> entry : map.entrySet()){
+            switch(entry.getKey()) {
+                case 'a':
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                    break;
+                case 'e':
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                    break;
+                case 'i':
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                    break;
+                case 'o':
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                    break;
+                case 'u':
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                    break;
+            }
+        }
+     }
+
+     public static void main(String[] args) {
+         countNumberOfVowels("Hello Niall How Are You");
+     }
+
+}
